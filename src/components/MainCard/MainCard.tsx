73e21b5e64823139fcd82493ef70a3b007b2c23f
joelbkoshy <joelbkoshy@gmail.com>
2024-02-27 23:00:20 +0530

    Added fade in MainCard

--- src/components/MainCard/MainCard.tsx
+++ src/components/MainCard/MainCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../../styles/MainCard.css';
 
 type CarouselItem = {
@@ -11,22 +11,32 @@ type MainCardProps = {
     carousel_data: CarouselItem[];
 };
 
+
 const MainCard: React.FC<MainCardProps> = ({ carousel_data }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const mainImgOverlay = useRef<any>(null);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % carousel_data.length);
-        }, 5000); 
+            mainImgOverlay.current.style.opacity = 1;
+            setTimeout(()=>{
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % carousel_data.length);
+            },400)
+        }, 5000);
 
         return () => clearInterval(intervalId);
     }, [carousel_data.length]);
+
+    useEffect(()=>{
+        mainImgOverlay.current.style.opacity = 0; 
+    },[currentIndex])
 
     return (
         <div className='mainCard-container'>
             <div className='mainCard-secondaryContainer'>
                 <div className="left rounded-20 shadow-white-mid border-white-mid">
-                    <img src={carousel_data[currentIndex].img_src} alt="" className='' />
+                    <div className="overlay" ref={mainImgOverlay}/>
+                    <img src={carousel_data[currentIndex].img_src} alt="" className='' id="main-img" />
                 </div>
                 <div className="right">
                     <div className="mainHeading">
