312bb79e403a9e3ba55e5b0255e396c4f66680f4
joelbkoshy <joelbkoshy@gmail.com>
2024-04-18 08:37:28 +0530

    Added enchristo styling image

--- src/pages/Enchristo.tsx
+++ src/pages/Enchristo.tsx
@@ -11,12 +11,18 @@ const Enchristo = () => {
       <div className="mainRelativeEnchristoContainer">
         <div className="mainEnchristoContainer">
           <div className="imgPlusTitleContainer">
-                <div className="enchristoImgContainer ">
-                  <img src={enchristo} className='rounded-20 shadow-white-mid border-white-mid' alt="" />
-                </div>
-                <div className="enchristoTitleContainer">
-                  <h2>hfrewfi</h2>
-                </div>
+            <div className="enchristoTitleContainer">
+              <div className="mainHeadingContainer">
+                <h1>Enchristo'24 : Faith and Friendship</h1>
+              </div>
+              <div className="subHeadingContainer">
+                <h6 className='dateText'>March 17,2024</h6>
+                <h6 className='venueText'>St Gaspar Bhavan, Precious Blood Missionaries</h6>
+              </div>
+            </div>
+            <div className="enchristoImgContainer ">
+              <img src={enchristo} className='rounded-20 shadow-white-mid border-white-mid' alt="" />
+            </div>
           </div>
         </div>
       </div>
