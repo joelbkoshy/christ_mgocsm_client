f8b7d7e7f4c4c11d2814ce3489afc3f98f74d4f2
joelbkoshy <joelbkoshy@gmail.com>
2024-07-29 01:34:44 +0530

    added about + contact

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,16 +1,38 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
+import '../styles/AboutUs.css'
 
 const About = () => {
   return (
     <div>
       <Navbar />
       <div className="aboutUsContainer">
-        
-        
+        <div className="imageContainer left rounded-20 shadow-white-mid border-white-mid">
+          <img src="/assets/about-us/bava.jpg" alt="" />
+        </div>
+        <div className="mgocsmContainer">
+          <h3>About MGOCSM</h3>
+          <p>
+            <span>
+              MAR GREGORIOS ORTHODOX CHRISTIAN STUDENT MOVEMENT
+              MGOCSM, the 1st Spiritual sapling for students in Asia, is the spiritual organization of the Malankara Orthodox SyrianChurch for its Student Community.
+
+              This spiritual organisation for the students was the laudable initiative of the Senior Leaders and students of our Church, in Madras, in the early part of the 20th century. The organization was founded on the basis of intense yearning for true spiritual progress for the youth along with an ardent spirit of Christian fellowship. The Organization was formed in 1907 as Syrian Student Conference. The first conference of the Movement was held at Madras on the 1st of January 1908.
+            </span>
+          </p>
+        </div>
+        <div className="aboutChristMgocsmContainer">
+          <h3>About CHRIST MGOCSM</h3>
+          <p>
+            <span>
+              The Mar Gregorios Orthodox Christian Student Movement (MGOCSM) in CHRIST University was founded in 2014 to support Orthodox Christian students in their spiritual and personal growth. The movement’s mission is to foster a vibrant Christian community through prayer, Bible study, and service, while its vision is to create an environment where students can deepen their faith and contribute positively to society.
+              The Bangalore Diocese has been instrumental in the movement’s establishment and growth. Under the leadership of H.G. Abraham Mar Seraphim Metropolitan, MGOCSM was launched with a focus on nurturing Orthodox Christian values among students. The guidance and support of the priests from the Bangalore Diocese have been crucial in shaping the movement’s activities and ensuring its alignment with Orthodox teachings.
+              Significant support has also come from the Catholic Archbishop Most Rev. Peter Machado; Carmelites of Mary Immaculate (CMI) Fathers, who run the University hosting the movement. The Chancellor, Vice-Chancellor, fathers and other religious nuns, in and around Koramangala, SG Palya have provided invaluable encouragement and resources, facilitating the movement’s integration into campus life and helping it thrive. Their commitment to supporting MGOCSM reflects the broader values of the University in fostering spiritual and personal development among its students. Through this collaborative effort, MGOCSM continues to serve as a cornerstone of spiritual engagement and community service for Orthodox Christian students.
+            </span>
+          </p>
+        </div>
         <h6>
-          CHRIST MGOCSM, which began its operations in 2014, celebrates its 10th anniversary this year, marking a decade of spiritual growth, community engagement, and service. Regular prayer sessions every Saturday at St. Thomas Forane Church and Holy Qurbana services at Block 2 Chapel and Precious Blood Missionaries nurture the spiritual well-being of members. Community meetups, including gatherings at various locations like St. Thomas Church, Block 4 Basement, Birds Park, and Pistachio Cafe, strengthen relationships and foster a sense of belonging. Special events such as farewell ceremonies, badge distributions, Carol Night, and meetings with dignitaries like His Grace Dr. Abraham Mar Seraphim Metropolitan enrich the community experience. Hosting deacons from the Orthodox Theological Seminary, festive gatherings like Christmas lights with Dennis Shemashen, and trips such as the memorable visit to Thadagam, Coimbatore, create cherished memories and deepen friendships. Musical jamming sessions and special prayers during Nineveh Lent and Great Lent periods provide spiritual sustenance and artistic expression. The highlight event, Enchristo'24, featured notable MGOCSM leaders and was widely recognized in the media. CHRIST MGOCSM remains steadfast in its mission to nurture the faith of its members and serve the community with love and compassion, looking forward to continuing this journey of faith and service in the years to come.
         </h6>
       </div>
       <Footer />
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
+import '../styles/Contact.css'; 
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
+      <div className="contactMainContainer">
+        <h1>Contact US</h1>
+      <div className="contactUs-Container">
+        <div className="contactSection">
+          <h2>MGOCSM Student Centre Bangalore</h2>
+          <p>Email: mgocsmstudentcenterbangalore@gmail.com</p>
+          <p>Phone: +919633738360</p>
+          <p>Fr. Jacob Varghese (Jikku achen)</p>
+        </div>
+        <div className="contactSection">
+          <h2>CHRIST MGOCSM</h2>
+          <p>Email: mgocsmchrist@gmail.com</p>
+          <p>Phone: 9747913779</p>
+          <p>Dn. Dennis Reji</p>
+          <p>Phone: +918078517439</p>
+          <p>Kripa Rachel Joji (Secretary)</p>
+          <p>Phone: +918547243014</p>
+          <p>Johan V Jacob</p>
+          <p>Phone: +918602255041</p>
+          <p>Rincy Aniyan Theshmeshtho (Choir)</p>
+        </div>
+      </div>
+
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
